1f9ea731b0004d59411238151ce057780c40fca3
Igor Gashenko <igor_4000@mail.ru>
2026-05-28 22:20:07 +0300

    Test discriminant function

--- Tests/test_t_one.py
+++ Tests/test_t_one.py
@@ -1,24 +1,21 @@
 import unittest
-
 from t_one import discriminant
 
 
 class TestDiscriminant(unittest.TestCase):
 
-    def test_two_roots(self):
-        """Уравнение  два корня"""
-        result = discriminant(1, -3, 2)
-        self.assertIsInstance(result, tuple)
-        self.assertEqual(len(result), 2)
-        self.assertEqual(result, (2.0, 1.0))
+    def test_discriminant(self):
+        """Параметризованный тест функции discriminant."""
+        cases = [
+            (1, -3, 2, (2.0, 1.0), "два корня"),
+            (1, -2, 1, 1.0, "один корень"),
+            (1, 1, 1, 'корней нет', "корней нет"),
+            (2, -8, 8, 2.0, "один корень (кратный)"),
+            (1, -5, 6, (3.0, 2.0), "два корня (целые)"),
+            (1, 0, -1, (1.0, -1.0), "два корня (b=0)"),
+        ]
 
-    def test_one_root(self):
-        """Уравнение один корень"""
-        result = discriminant(1, -2, 1)
-        self.assertIsInstance(result, float)
-        self.assertEqual(result, 1.0)
-
-    def test_no_roots(self):
-        """Уравнение корней нет"""
-        result = discriminant(1, 1, 1)
-        self.assertEqual(result, 'корней нет')
+        for a, b, c, expected, description in cases:
+            with self.subTest(msg=description):
+                result = discriminant(a, b, c)
+                self.assertEqual(result, expected)
